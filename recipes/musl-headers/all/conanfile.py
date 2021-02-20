94519b71c53e2d9c8122cc4fca22e10c10b098d8
Anton Danielsson <danielsson.anton@gmail.com>
2021-02-20 13:18:07 +0100

    [musl-headers] clean-up, CCI hook etc

--- recipes/musl-headers/all/conanfile.py
+++ recipes/musl-headers/all/conanfile.py
@@ -1,6 +1,6 @@
 import os
 from conans import ConanFile, tools
-from conans.errors import ConanException, ConanInvalidConfiguration
+
 
 class MuslHeadersConan(ConanFile):
     name = "musl-headers"
@@ -10,41 +10,34 @@ class MuslHeadersConan(ConanFile):
     description = ("musl is lightweight, fast, simple, free, and strives to be "
                    "correct in the sense of standards-conformance and safety.")
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "target": "ANY",
-        "shared": [True, False],
-        "fPIC": [True, False]
-    }
-    default_options = {
-        "target": None,
-        "shared": False,
-        "fPIC": True,
-    }
     topics = ("libc", "musl")
-    keep_imports = True
 
     @property
-    def _llvm_triplet(self):
-        arch = self.settings_target.arch # TODO: Translate names?
-        abi = 'musleabihf' # TODO: base on settings
+    def _target_arch(self):
+        # TODO: Need to translate conan/musl arch names?
+        settings_target = getattr(self, 'settings_target', None)
+        if settings_target is None:
+            settings_target = self.settings
+        return settings_target.arch
+
+    @property
+    def _triplet(self):
+        arch = self._target_arch
+        abi = 'musleabihf'  # TODO: base on settings
         return f"{arch}-linux-{abi}"
 
     def configure(self):
-        settings_target = getattr(self, 'settings_target', None)
-        if settings_target is None:
-            # It is running in 'host', so Conan is compiling this package
-            if not self.options.target:
-                raise ConanInvalidConfiguration("A value for option 'target' has to be provided")
-        else:
-            self.options.target = self._llvm_triplet
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.libcxx
 
-    def config_options(self):
-        # TODO: Check options
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        pass
+    def package_id(self):
+        # Even though this is a header only recipe the headers generated
+        # by make are different based on arch. So we need to include arch in
+        # the package id.
+        del self.info.settings.compiler
+        del self.info.settings.build_type
+        del self.info.settings.os
+        self.info.arch = self._target_arch
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -53,13 +46,11 @@ class MuslHeadersConan(ConanFile):
     def build(self):
         # TODO: winbash = true?
         with tools.chdir("musl"), \
-                tools.environment_append({"CFLAGS": f"--target={self.options.target}"}):
+                tools.environment_append({"CFLAGS": f"-target {self._triplet}"}):
             self.run(
-                f"./configure --target={self.options.target} --prefix={self.package_folder}")
+                f"./configure --target={self._triplet} --prefix={self.package_folder}")
             self.run("make install-headers")
 
     def package(self):
-        pass
-
-    def package_info(self):
-        pass
+        # Copy the license files
+        self.copy("COPYRIGHT", src="musl", dst="licenses")
