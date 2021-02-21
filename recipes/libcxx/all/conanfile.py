43b0114cd8baf4d69ebeeb125c74bf32c8f011b0
Anton Danielsson <danielsson.anton@gmail.com>
2021-02-21 23:15:21 +0100

    [libcxx] new style deps
    skip imports, instead use cpp_info as usual

--- recipes/libcxx/all/conanfile.py
+++ recipes/libcxx/all/conanfile.py
@@ -12,33 +12,14 @@ class ClangLibcxxConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
     topics = ("libc", "libcxx", "clang")
 
     @property
-    def _musl_abi(self):
-        # Translate arch to musl abi
-        abi = {"armv6": "musleabihf",
-               "armv7": "musleabi",
-               "armv7hf": "musleabihf"}.get(str(self.settings.arch))
-        # Default to just "musl"
-        if abi == None:
-            abi = "musl"
-
-        return abi
-
-    @property
-    def _musl_arch(self):
-        # Translate conan arch to musl/clang arch
-        arch = {"armv6": "arm",
-                "armv8": "aarch64"}.get(str(self.settings.arch))
-        # Default to a one-to-one mapping
-        if arch == None:
-            arch = str(self.settings.arch)
-        return arch
-
-    @property
     def _triplet(self):
-        return "{}-linux-{}".format(self._musl_arch, self._musl_abi)
+        return self.deps_cpp_info["musl-headers"].CHOST
 
     def _force_host_context(self):
         # If this recipe is a "build_requires" in the host profile we force
@@ -62,7 +43,6 @@ class ClangLibcxxConan(ConanFile):
         del self.info.settings.os_target
 
     def requirements(self):
-        self.requires("musl/1.2.2@dirac/testing")
         self.requires("libcxxabi/{}@dirac/testing".format(self.version))
 
     def config_options(self):
@@ -72,37 +52,31 @@ class ClangLibcxxConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("{}-{}.src".format(self.name, self.version), self.name)
+        os.rename("{}-{}.src".format(self.name, self.version), self._source_subfolder)
 
     def build(self):
-        # TODO: if unwinder
-        # We need to handle the dependencies and flags more "manual" because
-        # building this libs is a bit different from the normal stuff.
-        with tools.environment_append({"LDFLAGS": "-fuse-ld=lld -lcompiler_rt -L{}/lib".format(self.deps_cpp_info['libunwind-llvm'].rootpath)}):
-            cmake = CMake(self)
-            cmake.definitions["CMAKE_SYSROOT"] = self.deps_cpp_info["musl"].rootpath
-            cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_ASM_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_ASM_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
 
-            # TODO: base below on options
-            cmake.definitions["LIBCXX_HAS_MUSL_LIBC"] = True
-            cmake.definitions["LIBCXX_HAS_GCC_LIB"] = False
-            cmake.definitions["LIBCXX_HAS_GCC_S_LIB"] = False
-            cmake.definitions["LIBCXX_USE_COMPILER_RT"] = False
-            cmake.definitions["LIBCXX_CXX_ABI"] = "libcxxabi"
-            cmake.definitions["LIBCXXABI_USE_LLVM_UNWINDER"] = True
-            cmake.definitions["LIBCXX_CXX_ABI_INCLUDE_PATHS"] = os.path.join(
-                self.deps_cpp_info['libcxxabi'].rootpath, "include")
-            cmake.definitions["LIBCXX_CXX_ABI_LIBRARY_PATH"] = os.path.join(
-                self.deps_cpp_info['libcxxabi'].rootpath, "lib")
+        # TODO: base below on options
+        cmake.definitions["LIBCXX_HAS_MUSL_LIBC"] = True
+        cmake.definitions["LIBCXX_HAS_GCC_LIB"] = False
+        cmake.definitions["LIBCXX_HAS_GCC_S_LIB"] = False
+        cmake.definitions["LIBCXX_USE_COMPILER_RT"] = False
+        cmake.definitions["LIBCXX_CXX_ABI"] = "libcxxabi"
+        cmake.definitions["LIBCXXABI_USE_LLVM_UNWINDER"] = True
+        #cmake.definitions["LIBCXX_CXX_ABI_INCLUDE_PATHS"] = os.path.join(
+        #    self.deps_cpp_info['libcxxabi'].rootpath, "include")
+        #cmake.definitions["LIBCXX_CXX_ABI_LIBRARY_PATH"] = os.path.join(
+        #    self.deps_cpp_info['libcxxabi'].rootpath, "lib")
 
-            cmake.configure(source_folder=self.name,
-                            build_folder="{}-bin".format(self.name))
-            cmake.build()
-            cmake.install()
+        cmake.configure(build_folder="{}-bin".format(self.name))
+        cmake.build()
+        cmake.install()
 
     def package(self):
         # Copy the license files
-        self.copy("LICENSE.TXT", src=self.name, dst="licenses")
+        self.copy("LICENSE.TXT", src=self._source_subfolder, dst="licenses")
