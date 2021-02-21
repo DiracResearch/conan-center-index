8bf6ee517c95614461ffa7a468d8386a65b82aa0
Anton Danielsson <danielsson.anton@gmail.com>
2021-02-21 23:15:08 +0100

    [libcxxabi] new style deps
    skip imports, instead use cpp_info as usual

--- recipes/libcxxabi/all/conanfile.py
+++ recipes/libcxxabi/all/conanfile.py
@@ -12,41 +12,16 @@ class ClangLibcxxabiConan(ConanFile):
         "libc++abi is a new implementation of low level support for a standard C++ library.")
     license = "Apache License v2.0"
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False]
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True
-    }
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
     topics = ("libcxxabi", "clang")
 
     @property
-    def _musl_abi(self):
-        # Translate arch to musl abi
-        abi = {"armv6": "musleabihf",
-               "armv7": "musleabi",
-               "armv7hf": "musleabihf"}.get(str(self.settings.arch))
-        # Default to just "musl"
-        if abi == None:
-            abi = "musl"
-
-        return abi
-
-    @property
-    def _musl_arch(self):
-        # Translate conan arch to musl/clang arch
-        arch = {"armv6": "arm",
-                "armv8": "aarch64"}.get(str(self.settings.arch))
-        # Default to a one-to-one mapping
-        if arch == None:
-            arch = str(self.settings.arch)
-        return arch
-
-    @property
     def _triplet(self):
-        return "{}-linux-{}".format(self._musl_arch, self._musl_abi)
+        return self.deps_cpp_info["musl-headers"].CHOST
 
     def _force_host_context(self):
         # If this recipe is a "build_requires" in the host profile we force
@@ -62,6 +37,7 @@ class ClangLibcxxabiConan(ConanFile):
             self.settings.arch_target = settings_target.arch
             self.settings.os_target = settings_target.os
 
+
     def configure(self):
         self._force_host_context()
 
@@ -70,12 +46,11 @@ class ClangLibcxxabiConan(ConanFile):
         del self.info.settings.os_target
 
     def requirements(self):
-        self.requires("musl/1.2.2@dirac/testing")
         self.requires("libunwind-llvm/{}@dirac/testing".format(self.version))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("{}-{}.src".format(self.name, self.version), self.name)
+        os.rename("{}-{}.src".format(self.name, self.version), self._source_subfolder)
 
         # We need to fetch the headers for libcxx
         # Had this in "conandata.yml" before but CCI doesn't allow it.
@@ -86,29 +61,24 @@ class ClangLibcxxabiConan(ConanFile):
         os.rename("libcxx-{}.src".format(self.version), "libcxx")
 
     def build(self):
-        # We need to handle the dependencies and flags more "manual" because
-        # building this libs is a bit different from the normal stuff.
-        with tools.environment_append({"LDFLAGS": "-fuse-ld=lld -L{}/lib".format(self.deps_cpp_info['libunwind-llvm'].rootpath)}):
-            cmake = CMake(self)
-            cmake.definitions["CMAKE_SYSROOT"] = self.deps_cpp_info["musl"].rootpath
-            cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_ASM_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
-            cmake.definitions["LIBCXXABI_TARGET_TRIPLE"] = self._triplet
-            cmake.definitions["LIBCXXABI_USE_LLVM_UNWINDER"] = True
-            cmake.definitions["LIBCXXABI_USE_COMPILER_RT"] = True
-            cmake.definitions["LIBCXXABI_LIBCXX_INCLUDES"] = "{}/libcxx/include".format(
-                self.build_folder)
-            cmake.configure(source_folder=self.name,
-                            build_folder="{}-bin".format(self.name))
-            cmake.build()
-            cmake.install()
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_ASM_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
+        cmake.definitions["LIBCXXABI_TARGET_TRIPLE"] = self._triplet
+        cmake.definitions["LIBCXXABI_USE_LLVM_UNWINDER"] = True
+        cmake.definitions["LIBCXXABI_USE_COMPILER_RT"] = True
+        cmake.definitions["LIBCXXABI_LIBCXX_INCLUDES"] = "{}/libcxx/include".format(
+            self.build_folder)
+        cmake.configure(build_folder="{}-bin".format(self.name))
+        cmake.build()
+        cmake.install()
 
     def package(self):
         # Headers not installed by CMake, copy manually
         self.copy(pattern="*.h", dst="include",
-                  src="{}/include".format(self.name))
+                  src="{}/include".format(self._source_subfolder))
 
         # Copy the license files
-        self.copy("LICENSE.TXT", src=self.name, dst="licenses")
+        self.copy("LICENSE.TXT", src=self._source_subfolder, dst="licenses")
