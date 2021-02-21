0e5252ef52c23db97eccb1dd23562917ad6b0eb1
Anton Danielsson <danielsson.anton@gmail.com>
2021-02-21 23:14:27 +0100

    [lubunwind-llvm] new style deps
    skip imports, instead use cpp_info as usual

--- recipes/libunwind-llvm/all/conanfile.py
+++ recipes/libunwind-llvm/all/conanfile.py
@@ -11,41 +11,16 @@ class ClangUnwindConan(ConanFile):
     description = ("Unwind library from LLVM")
     exports_sources = ["CMakeLists.txt"]
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False]
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-    }
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
     topics = ("unwinder", "clang", "llvm")
 
     @property
-    def _musl_abi(self):
-        # Translate arch to musl abi
-        abi = {"armv6": "musleabihf",
-               "armv7": "musleabi",
-               "armv7hf": "musleabihf"}.get(str(self.settings.arch))
-        # Default to just "musl"
-        if abi == None:
-            abi = "musl"
-
-        return abi
-
-    @property
-    def _musl_arch(self):
-        # Translate conan arch to musl/clang arch
-        arch = {"armv6": "arm",
-                "armv8": "aarch64"}.get(str(self.settings.arch))
-        # Default to a one-to-one mapping
-        if arch == None:
-            arch = str(self.settings.arch)
-        return arch
-
-    @property
     def _triplet(self):
-        return "{}-linux-{}".format(self._musl_arch, self._musl_abi)
+        return self.deps_cpp_info["musl-headers"].CHOST
 
     def _force_host_context(self):
         # If this recipe is a "build_requires" in the host profile we force
@@ -73,24 +48,22 @@ class ClangUnwindConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("libunwind-{}.src".format(self.version), self.name)
+        os.rename("libunwind-{}.src".format(self.version), self._source_subfolder)
 
     def build(self):
-        with tools.environment_append({"LDFLAGS": "-fuse-ld=lld"}):
-            cmake = CMake(self)
-            cmake.definitions["CMAKE_SYSROOT"] = self.deps_cpp_info["musl"].rootpath
-            cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_ASM_COMPILER_TARGET"] = self._triplet
-            cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
-            cmake.definitions["LIBUNWIND_USE_COMPILER_RT"] = True
-            cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
-
-            cmake.configure(source_folder=self.name,
-                            build_folder="{}-bin".format(self.name))
-            cmake.build()
-            cmake.install()
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_C_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_CXX_COMPILER_TARGET"] = self._triplet
+        cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
+        cmake.definitions["LIBUNWIND_USE_COMPILER_RT"] = True
+        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
+        cmake.configure(build_folder="{}-bin".format(self.name))
+        cmake.build()
+        cmake.install()
 
     def package(self):
+        # Headers not installed by CMake, copy manually
+        self.copy(pattern="*.h", dst="include", src="{}/include".format(self._source_subfolder))
+
         # Copy the license files
-        self.copy("LICENSE.TXT", src=self.name, dst="licenses")
+        self.copy("LICENSE.TXT", src=self._source_subfolder, dst="licenses")
