c74996100ae336596ab23c80640e22bd10ab276a
Anton Danielsson <danielsson.anton@gmail.com>
2021-02-21 14:09:47 +0100

    [libunwind-llvm] new force_host_context logic

--- recipes/libunwind-llvm/all/conanfile.py
+++ recipes/libunwind-llvm/all/conanfile.py
@@ -22,44 +22,39 @@ class ClangUnwindConan(ConanFile):
     topics = ("unwinder", "clang", "llvm")
 
     @property
-    def _host_settings(self):
-        settings_target = getattr(self, 'settings_target', None)
-        if settings_target is None:
-            settings_target = self.settings
-        return settings_target
-
-    @property
-    def _conan_arch(self):
-        settings_target = getattr(self, 'settings_target', None)
-        if settings_target is None:
-            settings_target = self.settings
-        return settings_target.arch
-
-    @property
-    def _musl_abi(self):
-        # Translate arch to musl abi
-        abi = {"armv6": "musleabihf",
-               "armv7": "musleabi",
-               "armv7hf": "musleabihf"}.get(str(self._conan_arch))
-        # Default to just "musl"
-        if abi == None:
-            abi = "musl"
-
-        return abi
-
-    @property
     def _musl_arch(self):
         # Translate conan arch to musl/clang arch
         arch = {"armv6": "arm",
-                "armv8": "aarch64"}.get(str(self._conan_arch))
+                "armv8": "aarch64"}.get(str(self.settings.arch))
         # Default to a one-to-one mapping
         if arch == None:
-            arch = self._conan_arch
+            arch = str(self.settings.arch)
         return arch
 
     @property
     def _triplet(self):
         return "{}-linux-{}".format(self._musl_arch, self._musl_abi)
+
+    def _force_host_context(self):
+        # If this recipe is a "build_requires" in the host profile we force
+        # the settings to instead be settings_target.
+        # Ideally it should possible to express "force_host_context"
+        # under [build_requires] in the profile.
+        settings_target = getattr(self, 'settings_target', None)
+        if settings_target:
+            self.settings = settings_target
+            # For some reason we need to populate these manually
+            self.settings.arch_build = self.settings_build.arch
+            self.settings.os_build = self.settings_build.os
+            self.settings.arch_target = settings_target.arch
+            self.settings.os_target = settings_target.os
+
+    def configure(self):
+        self._force_host_context()
+
+    def package_id(self):
+        del self.info.settings.arch_target
+        del self.info.settings.os_target
 
     def requirements(self):
         self.requires("musl/1.2.2@dirac/testing")
@@ -87,15 +82,3 @@ class ClangUnwindConan(ConanFile):
     def package(self):
         # Copy the license files
         self.copy("LICENSE.TXT", src=self.name, dst="licenses")
-
-    def package_id(self):
-        # Copy settings from host env.
-        # Need to do this to be able to have llvm-sysroot in "build_requires" in the profile
-        self.info.settings.clear()
-        self.info.settings.compiler = self._host_settings.compiler
-        self.info.settings.compiler.version = self._host_settings.compiler.version
-        self.info.settings.compiler.libc = self._host_settings.compiler.libc
-        self.info.settings.compiler.libcxx = self._host_settings.compiler.libcxx
-        self.info.settings.build_type = self._host_settings.build_type
-        self.info.settings.os = self._host_settings.os
-        self.info.settings.arch = self._host_settings.arch
